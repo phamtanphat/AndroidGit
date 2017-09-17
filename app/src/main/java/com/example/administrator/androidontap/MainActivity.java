1fb45158b46352d2b4b4bbabe96fe140d8d6050f
phamtanphat <phatdroid94@gmail.com>
2017-09-17 10:06:22 +0700

    commit 1
    commit
    
    example

---
+++ app/src/main/java/com/example/administrator/androidontap/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.administrator.androidontap;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.d("BBB","Chao ban");
+        Log.d("BBB","BBB");;;;;;;;;
+        //Kiem file được thay đổi
+//            git add . (Lưu giá trị thay đổi)
+        //Lưu gía trị thay đổi của project
+//            git commit
+        //Kiểm tra có thay đổi gì về file
+//            git status
+        //Comit code lại (Sử dụng -am để tên comit cho file mới)
+//            git commit -am "tencommit"
+           // Loi khi bi private email
+//                setting -> email -> unselect check box block command line
+            //Dua code len github
+//                git remote add origin "link project git"
+            //Push code len git
+//                git push origin tennhanh
+            //Clone code tu git
+//                git clone duongdanproject tu git
+            //Kiem tra du lieu trong project ve(cd de thu muc chua project clone ve)
+//                git checkout tennhanh(view code nhanh nao thi truyen vao)
+            //Lay nhanh chua dua remote(Chi tao tren github chu chua tao tren local)
+//                git fecth -all
+            //Neu muon lay 1 nhanh nao chua co tren local
+//                git fecth origin tennhanh(Test git checkout tennhanh)
+
+    }
+}
